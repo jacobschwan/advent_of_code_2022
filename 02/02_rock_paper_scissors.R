f64b2909c6881a03bf3297415bd7483d0d6e593e
Jacob Schwan <jschwan@mitchell.local.schwan.family>
2022-12-02 11:09:00 -0600

    switch lookup tibbles to lookup vectors

--- 02/02_rock_paper_scissors.R
+++ 02/02_rock_paper_scissors.R
@@ -1,24 +1,16 @@
 library(tidyverse)
 
-opp_key_tbl <- tribble(
-    ~opp_key,  ~opp,
-    "A",       "rock",
-    "B",       "paper",
-    "C",       "scissors"
-)
+opp_key_vec <- c("A" = "rock",
+                 "B" = "paper",
+                 "C" = "scissors")
 
-me_key_tbl <- tribble(
-    ~me_key,   ~me,
-    "X",       "rock",
-    "Y",       "paper",
-    "Z",       "scissors"
-)
+me_key_vec <- c("X" = "rock",
+                "Y" = "paper",
+                "Z" = "scissors")
 
-play_points_fn <- function(me) {
-    case_when(me == "rock" ~ 1,
-              me == "paper" ~ 2,
-              me == "scissors" ~ 3)
-}
+shape_points_vec <- c("rock"     = 1,
+                      "paper"    = 2,
+                      "scissors" = 3)
 
 input_path <- here::here("02/input.txt")
 
@@ -26,9 +18,9 @@ input_tbl <- read_delim(input_path, delim = " ", col_names = c("opp_key", "me_ke
 
 # Part 1
 outcome_tbl <- input_tbl %>%
-    left_join(opp_key_tbl, by = c("opp_key")) %>%
-    left_join(me_key_tbl, by = c("me_key")) %>%
-    mutate(play_points = play_points_fn(me),
+    mutate(opp = opp_key_vec[opp_key],
+           me = me_key_vec[me_key],
+           play_points = shape_points_vec[me],
            outcome_points = case_when(me == opp ~ 3,
                                       me == "rock" & opp == "scissors" ~ 6,
                                       me == "paper" & opp == "rock" ~ 6,
@@ -39,24 +31,26 @@ outcome_tbl <- input_tbl %>%
 sum(outcome_tbl$total_points)
 
 # Part 2
-outcome_key_tbl <- tribble(
-    ~outcome_key, ~outcome_points,
-    "X",          0,
-    "Y",          3,
-    "Z",          6
-)
+outcome_key_vec <- c("X" = 0,
+                     "Y" = 3,
+                     "Z" = 6)
 
-outcome2_tbl <- outcome_key_tbl %>%
-    right_join(input_tbl, by = c("outcome_key" = "me_key")) %>%
-    left_join(opp_key_tbl, by = c("opp_key")) %>%
-    mutate(me = case_when(outcome_points == 3 ~ opp,
-                          outcome_points == 6 ~ case_when(opp == "rock" ~ "paper",
-                                                          opp == "paper" ~ "scissors",
-                                                          opp == "scissors" ~ "rock"),
-                          outcome_points == 0 ~ case_when(opp == "rock" ~ "scissors",
-                                                          opp == "paper" ~ "rock",
-                                                          opp == "scissors" ~ "paper")),
-           play_points = play_points_fn(me),
+# Vector of wining plays
+#            To Beat      Play 
+win_vec <- c("rock"     = "paper",
+             "paper"    = "scissors",
+             "scissors" = "rock")
+
+# Losing plays are the opposite, so switch values & names
+lose_vec <- set_names(names(win_vec), win_vec)
+
+outcome2_tbl <- input_tbl %>%
+    mutate(opp = opp_key_vec[opp_key],
+           outcome_points = outcome_key_vec[me_key],
+           me = case_when(outcome_points == 3 ~ opp,
+                          outcome_points == 6 ~ win_vec[opp],
+                          outcome_points == 0 ~ lose_vec[opp]),
+           play_points = shape_points_vec[me],
            total_points = play_points + outcome_points)
 
 sum(outcome2_tbl$total_points)
