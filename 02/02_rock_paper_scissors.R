238b4e5b50bd6a4892ad7a5acc3ab446d80d151b
Jacob Schwan <jschwan@mitchell.local.schwan.family>
2022-12-02 10:31:13 -0600

    Part 2 solutions

--- 02/02_rock_paper_scissors.R
+++ 02/02_rock_paper_scissors.R
@@ -1,7 +1,3 @@
-# A == "rock"      X == "rock"
-# B == "paper"     Y == "paper"
-# C == "scissors"  Z == "scissors"
-
 library(tidyverse)
 
 opp_key_tbl <- tribble(
@@ -18,16 +14,21 @@ me_key_tbl <- tribble(
     "Z",       "scissors"
 )
 
+play_points_fn <- function(me) {
+    case_when(me == "rock" ~ 1,
+              me == "paper" ~ 2,
+              me == "scissors" ~ 3)
+}
+
 input_path <- here::here("02/input.txt")
 
 input_tbl <- read_delim(input_path, delim = " ", col_names = c("opp_key", "me_key"))
 
+# Part 1
 outcome_tbl <- input_tbl %>%
     left_join(opp_key_tbl, by = c("opp_key")) %>%
     left_join(me_key_tbl, by = c("me_key")) %>%
-    mutate(play_points = case_when(me == "rock" ~ 1,
-                                   me == "paper" ~ 2,
-                                   me == "scissors" ~ 3),
+    mutate(play_points = play_points_fn(me),
            outcome_points = case_when(me == opp ~ 3,
                                       me == "rock" & opp == "scissors" ~ 6,
                                       me == "paper" & opp == "rock" ~ 6,
@@ -36,3 +37,26 @@ outcome_tbl <- input_tbl %>%
            total_points = play_points + outcome_points)
 
 sum(outcome_tbl$total_points)
+
+# Part 2
+outcome_key_tbl <- tribble(
+    ~outcome_key, ~outcome_points,
+    "X",          0,
+    "Y",          3,
+    "Z",          6
+)
+
+outcome2_tbl <- outcome_key_tbl %>%
+    right_join(input_tbl, by = c("outcome_key" = "me_key")) %>%
+    left_join(opp_key_tbl, by = c("opp_key")) %>%
+    mutate(me = case_when(outcome_points == 3 ~ opp,
+                          outcome_points == 6 ~ case_when(opp == "rock" ~ "paper",
+                                                          opp == "paper" ~ "scissors",
+                                                          opp == "scissors" ~ "rock"),
+                          outcome_points == 0 ~ case_when(opp == "rock" ~ "scissors",
+                                                          opp == "paper" ~ "rock",
+                                                          opp == "scissors" ~ "paper")),
+           play_points = play_points_fn(me),
+           total_points = play_points + outcome_points)
+
+sum(outcome2_tbl$total_points)
